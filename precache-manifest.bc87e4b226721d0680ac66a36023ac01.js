483afa43a144ef5464ff2641d02d20b96bf10eec
Rahamirez Galinski <rahamirez.ga@gmail.com>
2019-09-11 11:12:24 +0700

    Updates

---
+++ precache-manifest.bc87e4b226721d0680ac66a36023ac01.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecea20f63b9ceef15923d61c75588ca",
+    "url": "/potamir/KoneKita.git/index.html"
+  },
+  {
+    "revision": "6b4a2a108f8c0f626d61",
+    "url": "/potamir/KoneKita.git/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "e4ef6c6920e2bac3cf42",
+    "url": "/potamir/KoneKita.git/static/css/main.511873aa.chunk.css"
+  },
+  {
+    "revision": "6b4a2a108f8c0f626d61",
+    "url": "/potamir/KoneKita.git/static/js/2.95452956.chunk.js"
+  },
+  {
+    "revision": "e4ef6c6920e2bac3cf42",
+    "url": "/potamir/KoneKita.git/static/js/main.3fbbc3a5.chunk.js"
+  },
+  {
+    "revision": "2834babf705fd303f095",
+    "url": "/potamir/KoneKita.git/static/js/runtime~main.3881418e.js"
+  },
+  {
+    "revision": "360d623ee72d0a14298ef2d7cb391480",
+    "url": "/potamir/KoneKita.git/static/media/Astro_2@1X.360d623e.png"
+  },
+  {
+    "revision": "3aa418bdd950d5a375664785b6e8a598",
+    "url": "/potamir/KoneKita.git/static/media/astro_1@1X.3aa418bd.png"
+  },
+  {
+    "revision": "75bfabac3cad9cf139532f91a316dfb9",
+    "url": "/potamir/KoneKita.git/static/media/back-agency-planet1@1X.75bfabac.png"
+  },
+  {
+    "revision": "56d9adf5622eb404f94317a969b44e80",
+    "url": "/potamir/KoneKita.git/static/media/back-agency@1X.56d9adf5.png"
+  },
+  {
+    "revision": "1efee87b594f5b24f90a02f21e0ed6c4",
+    "url": "/potamir/KoneKita.git/static/media/back-end-planet1@1X.1efee87b.png"
+  },
+  {
+    "revision": "934df4422a1b501107c31d3ebd586c70",
+    "url": "/potamir/KoneKita.git/static/media/back-end-planet@1X.934df442.png"
+  },
+  {
+    "revision": "c44cbe0f54c6b755f9d99de98d35ac22",
+    "url": "/potamir/KoneKita.git/static/media/back-home-plane2@1X.c44cbe0f.png"
+  },
+  {
+    "revision": "bce8b3f93d3b58b954bfb0885fcd5685",
+    "url": "/potamir/KoneKita.git/static/media/back-home-planet3@1X.bce8b3f9.png"
+  },
+  {
+    "revision": "a1eaf9ffda862b771890da0f9157882f",
+    "url": "/potamir/KoneKita.git/static/media/back-home-planet4@1X.a1eaf9ff.png"
+  },
+  {
+    "revision": "70d8341966c7ad9b01ba8f57d29b1990",
+    "url": "/potamir/KoneKita.git/static/media/back-home@1X.70d83419.png"
+  },
+  {
+    "revision": "58a9b21c1cf9bae51f514f803676e81e",
+    "url": "/potamir/KoneKita.git/static/media/back-pekarya-line@1X.58a9b21c.png"
+  },
+  {
+    "revision": "9cfd0425f1f0a97d11416fa30b552742",
+    "url": "/potamir/KoneKita.git/static/media/back-pekarya-planet1@1X.9cfd0425.png"
+  },
+  {
+    "revision": "a98d105b1528c5c02ca1282cc3e17d64",
+    "url": "/potamir/KoneKita.git/static/media/back-pekarya@1X.a98d105b.png"
+  },
+  {
+    "revision": "355233fca61e11dfb1d012b9d507fbda",
+    "url": "/potamir/KoneKita.git/static/media/back-who-planet1@1X.355233fc.png"
+  },
+  {
+    "revision": "cdf77d57870d863d3890f0d964fef7d9",
+    "url": "/potamir/KoneKita.git/static/media/back-who-planet2@1X.cdf77d57.png"
+  },
+  {
+    "revision": "87a18a0b2b27e1d66d70255a32d2b3fb",
+    "url": "/potamir/KoneKita.git/static/media/back-who@1X.87a18a0b.png"
+  },
+  {
+    "revision": "3bc937ab4b4b4a3586b69baf5eca546f",
+    "url": "/potamir/KoneKita.git/static/media/back-why-planet4@1X.3bc937ab.png"
+  },
+  {
+    "revision": "b41dc218cbcffc4ae9c8d540fc5f6427",
+    "url": "/potamir/KoneKita.git/static/media/back-why-planet@1X.b41dc218.png"
+  },
+  {
+    "revision": "1565f38459df60cae915da93da2223c3",
+    "url": "/potamir/KoneKita.git/static/media/icon-bayar@1X.1565f384.png"
+  },
+  {
+    "revision": "348f628deb7682a56acdc1c132b95558",
+    "url": "/potamir/KoneKita.git/static/media/icon-integrasi@1X.348f628d.png"
+  },
+  {
+    "revision": "fed4747989f5446afa272871efceed29",
+    "url": "/potamir/KoneKita.git/static/media/icon-kolab@1X.fed47479.png"
+  },
+  {
+    "revision": "8c110c0d4f00a7ac87f382ad5d40520d",
+    "url": "/potamir/KoneKita.git/static/media/icon-kurasi@1X.8c110c0d.png"
+  },
+  {
+    "revision": "e722c5582ede481abf58fa4e88eb5b8c",
+    "url": "/potamir/KoneKita.git/static/media/icon-realtime@1X.e722c558.png"
+  },
+  {
+    "revision": "e1c3d236a503f0664bc414a34b5faa8c",
+    "url": "/potamir/KoneKita.git/static/media/icon-temukan@1X.e1c3d236.png"
+  },
+  {
+    "revision": "46904dd54f8dbb72532f32436ee3c567",
+    "url": "/potamir/KoneKita.git/static/media/icon-ukur@1X.46904dd5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d24bd0d9c434e319d29c870f5ae5b4fa.js"
+  "/potamir/KoneKita.git/precache-manifest.bc87e4b226721d0680ac66a36023ac01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/potamir/KoneKita.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
