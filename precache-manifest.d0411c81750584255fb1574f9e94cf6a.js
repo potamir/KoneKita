02512be5015aa82cb40be04236c9e091b3204324
Rahamirez Galinski <rahamirez.ga@gmail.com>
2019-09-23 17:47:16 +0700

    Updates

---
+++ precache-manifest.d0411c81750584255fb1574f9e94cf6a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e03aa1601a504e8d0bcbe7b01516fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "401aa0c050fc88de8494",
+    "url": "./static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "37e8cb490657720afb93",
+    "url": "./static/css/main.72135883.chunk.css"
+  },
+  {
+    "revision": "401aa0c050fc88de8494",
+    "url": "./static/js/2.bb732fad.chunk.js"
+  },
+  {
+    "revision": "37e8cb490657720afb93",
+    "url": "./static/js/main.17501510.chunk.js"
+  },
+  {
+    "revision": "04bfc2c843323aa33047",
+    "url": "./static/js/runtime~main.320ab26b.js"
+  },
+  {
+    "revision": "44dfe8cc676882243911a3197a50169e",
+    "url": "./static/media/Lato-Bold.44dfe8cc.ttf"
+  },
+  {
+    "revision": "5b761f2d1e4259ea6ac7ab3ebf7f3c49",
+    "url": "./static/media/Lato-Light.5b761f2d.ttf"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "./static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "./static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "./static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "./static/media/Roboto-Regular.df7b648c.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.228998f3f5c2a16fdd08cc6b0e2720d6.js"
+  "./precache-manifest.d0411c81750584255fb1574f9e94cf6a.js"
 );
 
 self.addEventListener('message', (event) => {
